994b26f617ff4651f5ad24c2791d3276dd622e43
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-06-19 15:14:15 +0200

    Flow : first commit & Testing datas front end

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,42 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+
+    const { name, portrait, city, tagline,price } = data;
+
+    // console.log(data);
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+
         const article = document.createElement( 'article' );
+        article.classList.add('card','card-photographer')
+
         const img = document.createElement( 'img' );
+        img.classList.add('thumb-photographer')
         img.setAttribute("src", picture)
+
         const h2 = document.createElement( 'h2' );
+        h2.classList.add('title-photographer')
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        
+
+        const cityName = document.createElement('span');
+        cityName.classList.add('photographer-city');
+        cityName.textContent = `${city}`
+
+
+        const taglineText = document.createElement('span');
+        taglineText.classList.add('photographer-tagline');
+         taglineText.textContent = ` 👉 ${tagline}`;
+
+         const pricingRange = document.createElement('p');
+         pricingRange .classList.add('photographer-pricing');
+        pricingRange .textContent = `${price} euros`;
+
+        article.append(img,h2,pricingRange,cityName,taglineText,);
+
         return (article);
     }
+
     return { name, picture, getUserCardDOM }
 }
